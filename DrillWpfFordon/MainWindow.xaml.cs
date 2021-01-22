fdca6cfd8ad137e2c065041a98bd204a6739c73f
Karlsson0214 <karlsson0214@gmail.com>
2021-01-22 09:44:11 +0100

    Add project files.

---
+++ DrillWpfFordon/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DrillWpfFordon
+{ 
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // lista med alla fordon
+        private List<Vehicle> vehicles = new List<Vehicle>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            // skriv kod här för att spara fordon i lista
+
+        }
+    }
+}
---
+++ DrillWpfFordon/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrillWpfFordon
+{
+    class Vehicle
+    {
+        // registratoin number
+        // owner
+
+
+        // konstruktor tar registeringsnummer som parameter
+        // bara registreringsnummer
+
+        // property för att läsa registreringsnummer
+
+        // property för att läsa och sätta ägare
+    }
+}
